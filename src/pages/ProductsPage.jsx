d9c8c5f12049a7e6aad5ac5e919cea4d2fa9884b
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-11-14 15:09:12 +0330

    handled filtering & query string

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,23 +1,56 @@
 // hooks
 import { useProducts } from "../context/ProductsProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 // components
 import Card from "../component/Card";
 import Loader from "../component/Loader";
+// helpers
+import {
+  createQueryObject,
+  filterProducts,
+  getInitQuery,
+  searchProducts,
+} from "../helpers/helper";
 // CSS
 import styles from "./ProductsPage.module.css";
 // icons
 import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 
 function ProductsPage() {
   // states
-  const [search, setSearch] = useState("");
   const products = useProducts();
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // effects
+  useEffect(() => {
+    setDisplayed(products);
+    setQuery(getInitQuery(searchParams));
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "")
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
 
   // handlers
   const searchHandler = () => {
-    
-  }
+    setQuery((prevQuery) => createQueryObject(prevQuery, { search }));
+  };
+
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = event.target?.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    setQuery((prevQuery) => createQueryObject(prevQuery, { category }));
+  };
 
   return (
     <>
@@ -34,12 +67,24 @@ function ProductsPage() {
       </div>
       <div className={styles.container}>
         <div className={styles.products}>
-          {!products.length && <Loader />}
-          {products.map((p) => (
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
             <Card key={p.id} data={p} />
           ))}
         </div>
-        <div>sidebar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
     </>
   );
